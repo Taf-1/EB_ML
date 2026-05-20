8d995d260f28bb28b7475973821aa8fc27b8cc59
Taf-1 <tafadzwazivave@gmail.com>
2026-05-20 12:27:18 +0100

    currently running initial query to get variable table, which i will then update bg_ml.py

--- bg_main.py
+++ bg_main.py
@@ -13,6 +13,7 @@ import numpy as np
 from astropy.coordinates import SkyCoord
 from astroquery.skyview import SkyView
 import astropy.units as u
+import pandas as pd
 
 def arg_parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Run the BG pipeline")
@@ -40,13 +41,13 @@ def main():
     wd_table = cfg["wd_table"]
     hr_table = cfg["hr_table"]
     logger = bg_logger.BG_logging(stage_name="BG_pipeline", log_file=os.path.join(data_root, "logs/bg_pipeline.log")).setup_logger()
-    bg_query.Google_Cloud_query(logger, variable_table_loc, project_id, os.path.join(data_root, "variable_table.fits"))
+    bg_query.Google_Cloud_query(logger, variable_table_loc, project_id, os.path.join(data_root, "variable_table.fits")).run_query()
     bg_stack_lcs.Stack_LCS(logger, os.path.join(data_root, "variable_table.fits")).stack_light_curves()
     wd_files = [os.path.join(data_root, "lightcurves", f) for f in os.listdir(os.path.join(data_root, "lightcurves")) if f.endswith("_LC.fits")]
+    per_wd_results = []
     for wd_file in wd_files:
         logger.info(f"Processing light curve file: {wd_file}")
-        wd_prefix = os.path.basename(wd_file).replace("_LC.fits", "")
-        wd_name = wd_prefix.replace(f"{data_root}/lightcurves/", "")
+        wd_name = os.path.basename(wd_file).replace("_LC.fits", "")
         wd_dir = os.path.join(data_root, "analysis", wd_name)
         logger.info(f"Creating directory for analysis of {wd_name}: {wd_dir}")
         if not os.path.exists(wd_dir):
@@ -73,6 +74,7 @@ def main():
             dss_image = SkyView.get_images(position=target_sky_coords, survey=['DSS'], radius=50*u.arcsec)
         logger.info("DSS images collected - transforming to data to be used later on ...")
         dss_data = dss_image[0][0].data if len(dss_image) > 0 else None
+        per_filter_data = {}
         for filt in filters:
             logger.info(f"Processing filter {filt} for Gaia ID {gaia_id}")
             mask = lc_table["FILTER"] == filt
@@ -86,7 +88,7 @@ def main():
             logger.info(f"Flux has been normalized - max flux - {max(ratio_norm)} min flux - {min(ratio_norm)}")
             logger.info("Ready to conduct the period search using BLS and LS algorithms")
             results_filename = f"{wd_name}_{filt}_res.txt"
-            analysis_initialise = bg_analysis(logger=logger, time=time, flux=ratio_norm, filename=results_filename)
+            analysis_initialise = bg_analysis.BG_analysis(logger=logger, time=time, flux=ratio_norm, filename=results_filename)
             logger.info("Initialised the BG_analysis - first stage: Running the BLS period search")
             bls_params = analysis_initialise.run_bls()
             logger.info(f"BLS period search complete - BLS params dict includes - {bls_params}")
@@ -100,14 +102,28 @@ def main():
                         time=time, flux=ratio_norm, dss_data=dss_data, bls_params=bls_params,
                         ls_params=ls_params, nic_data=wd_table, gaia_id=gaia_id, 
                         output_filename=BG_output_filename, telescope='BG', hr_table=hr_table).bg_diagnostic_plot()
+            """Will add TESS followup later on - for now just create the BG diagnostic plot and save the BLS and LS params to a csv file for ML"""
             """logger.info(f"Period search completed - creating the diagnostic plots - {TESS_output_filename}")
             bg_plotting.diagnostic_plotting(logger=logger, gcs_files=gcs_files, ra_deg=ra_deg, dec_deg=dec_deg,
                         time=time, flux=ratio_norm, dss_data=dss_data, bls_params=bls_params,
                         ls_params=ls_params, nic_data=wd_table, gaia_id=gaia_id, 
                         output_filename=TESS_output_filename, telescope='TESS', hr_table=hr_table).bg_diagnostic_plot()"""
-            """
-            APPEND THE BLS_PARAMS, LS_PARAMS, GAIA_ID AND FILTER then save the total list to a csv file for ML
-            """
+            logger.info(f"Diagnostic plots created for filter {filt} of Gaia ID {gaia_id} - now saving the BLS and LS params to a dictionary for ML")
+            per_filter_data[filt] = {
+                "bls_params": bls_params,
+                "ls_params": ls_params,
+                "gaia_id": gaia_id,
+                "filter": filt
+            }
+        logger.info(f"Completed processing for Gaia ID {gaia_id} - now appending the per filter data to the per WD results list")
+        per_wd_results.append(per_filter_data)
+        """APPEND THE BLS_PARAMS, LS_PARAMS, GAIA_ID AND FILTER then save the total list to a csv file for ML"""
+    """save out the per_wd_results list to a csv file for ML"""
+    per_wd_results_filename = os.path.join(data_root, "per_wd_results.csv")
+    logger.info(f"Saving the per WD results to {per_wd_results_filename}")
+    per_wd_results_df = pd.DataFrame(per_wd_results)
+    per_wd_results_df.to_csv(per_wd_results_filename, index=False)
+    logger.info(f"Saved the per WD results to {per_wd_results_filename} - BG pipeline complete")
 
 if __name__ == "__main__":
     main()
